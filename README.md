17e6a56ea698a7c7c4b6cd6357533f029695aa41
Reto Aebersold <aeby@substyle.ch>
2018-07-26 20:36:00 -0600

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# ald
+
+AWS Lambda Deployment Test
+
+[![Build Status](https://travis-ci.org/aeby/ald.svg?branch=master)](https://travis-ci.org/aeby/ald)
---
+++ phone_echo.py
@@ -0,0 +1,49 @@
+import boto3
+import json
+from boto3.dynamodb.conditions import Key
+
+from twilio.twiml.voice_response import VoiceResponse
+
+dynamo = boto3.client('dynamodb')
+table = dynamo.Table('recording')
+
+response = table.query(
+    ProjectionExpression='phone, ts, recording',
+    KeyConditionExpression=Key('phone').eq('+4146545546')
+)
+
+for i in response['Items']:
+    print(json.dumps(i))
+
+
+def echo_handler(event, ctx):
+    operation = event['httpMethod']
+    response = VoiceResponse()
+
+    if operation == 'GET':
+        # Check if we have a recording in the past 10s from the calling number
+        if False:
+            response.say('Playing your recorded message')
+            response.play('https://api.twilio.com/cowbell.mp3')
+            response.say(
+                'If you are able hear your own voice, the Caru phone is working correctly. '
+                'If you hear this message but not your own voice then something is wrong with the audio settings. '
+                'Please check the sensor logs in your dashboard.')
+            response.hangup()
+        else:
+            # Record a new message
+            response.say(
+                'Hello. Welcome to the Caru phone testing service. After the beep please record the message. '
+                'On your next call the message will be played back to you.')
+            # This calls this URL with a POST request
+            response.record(max_length=5)
+    elif operation == 'POST':
+        # Save recording url for calling number
+        data = json.loads(event['body'])
+        recording = data['RecordingUrl']
+        caller = data['From']
+
+        pass
+
+    response.hangup()
+    return str(response)
